bcef1e01d61a4a9f683e41f2e36b7eca21bb176c
alex chalyy <alexchalyy@gmail.com>
2019-02-22 15:11:58 -0500

    java script started

---
+++ assets/javascript/app.js
@@ -0,0 +1,19 @@
+/*  This javascript uses jQueries to execute the quiz.
+
+    Written by Alex Chalyy on 2/21/2019.    */
+
+    function Timer(c)  {
+        $("#time").html("Time Remaining: " + c + " seconds");
+        console.log("Time: " + c + " seconds.");
+    }
+
+    $("#start").click(
+        function Time() {
+            
+            console.log("Timer started");
+            for (var c = 30; c > 0; c--)    {
+                setTimeout(Timer(c), 1000);
+            }
+        }
+    );
+    
