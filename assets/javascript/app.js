7999d4a609f7e19c0d52efd4812d5abef82ebe77
alex chalyy <alexchalyy@gmail.com>
2019-02-22 19:53:20 -0500

    timer added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,7 @@
 /*  This javascript uses jQueries to execute the quiz.
 
     Written by Alex Chalyy on 2/22/2019.    */
-
+/*
     function Timer(c)  {
         $("#time").html("Time Remaining: " + c + " seconds");
         console.log("Time: " + c + " seconds.");
@@ -16,4 +16,78 @@
             }
         }
     );
-    
+    */
+/*
+var c = 30;
+
+function Timer() {
+
+    $("#time").html("Time Remaining: " + c + " seconds");
+    console.log("Time: " + c + " seconds.");
+    c--;
+}
+
+$("#start").click(
+    function Time() {
+        while (c > 0)  {
+
+        }
+        
+    });
+*/
+
+//  Interval Demonstration
+    //  Set our number counter to 100.
+    var number = 30;
+
+    //  Variable that will hold our interval ID when we execute
+    //  the "run" function
+    var intervalId;
+
+    //  When the stop button gets clicked, run the stop function.
+    //$("#stop").on("click", stop);
+
+    //  When the resume button gets clicked, execute the run function.
+    $("#start").on("click", run);
+
+    //  The run function sets an interval
+    //  that runs the decrement function once a second.
+    //  *****BUG FIX******** 
+    //  Clearing the intervalId prior to setting our new intervalId will not allow multiple instances.
+    function run() {
+      clearInterval(intervalId);
+      intervalId = setInterval(decrement, 1000);
+    }
+
+    //  The decrement function.
+    function decrement() {
+
+      //  Decrease number by one.
+      number--;
+
+      //  Show the number in the #show-number tag.
+      $("#sec").html(number);
+
+
+      //  Once number hits zero...
+      if (number === 0) {
+
+        //  ...run the stop function.
+        stop();
+
+        //  Alert the user that time is up.
+        alert("Time Up!");
+      }
+    }
+
+    //  The stop function
+    function stop() {
+
+      //  Clears our intervalId
+      //  We just pass the name of the interval
+      //  to the clearInterval function.
+      clearInterval(intervalId);
+    }
+
+    //  Execute the run function.
+    run();
